cb7a9811e21af83be6f388df397e85237609f3bc
Matilde Park <m@park.computer>
2022-04-28 18:43:28 -0700

    CommentItem: transparent background

--- pkg/interface/src/views/components/CommentItem.tsx
+++ pkg/interface/src/views/components/CommentItem.tsx
@@ -161,7 +161,7 @@ return false;
         p={1}
         mb={1}
         ml="28px"
-        backgroundColor={highlighted ? 'washedBlue' : 'white'}
+        backgroundColor={highlighted ? 'washedBlue' : 'transparent'}
         transcluded={0}
         contents={post.contents}
         showOurContact
